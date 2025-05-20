037897a857577426cf7c4da08569566521a00d83
Olivia Putri <oliviaputri406@gmail.com>
2025-05-21 01:24:38 +0700

    new change olip

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/register_screen.dart';
 
 void main() {
-  runApp(
-   
-       MyApp(),
-    
-  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
-      
+      initialRoute: '/',
+      routes: {
+        '/': (context) => RegisterPage(),
+        '/register': (context) => RegisterPage(),
+        '/login': (context) => LoginPage(),
+        '/home': (context) => HomeScreen(),
+      },
     );
   }
 }
